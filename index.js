24e0f739691d62963bdb4dec7ecc54fceeedc7d1
Siddharth Shyniben <siddhart.muscat@gmail.com>
2024-04-08 11:17:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let l='length',T=(k,v='string')=>typeof k==v,V=(A,B='')=>t=>Object.entries(A).map(([k,v])=>{if(Object.prototype.toString.call(v)=='[object Object]')return V(v,k+'.')(t[k]||{});else if(Array.isArray(v))v.map(f=>{if(T(f=f(t[k],B+k)))throw new Error(f)});else if(T(v=v(t[k],B+k)))throw new Error(v)})&&1,_=Z=>(x,k)=>x!==void 0&&x!==null?Z(x,k):1,Y=(m,k,t)=>m||k+' must be '+t,J=x=>Array.isArray(x)?'item':'character'
+V.required=m=>(x,k)=>x!==void 0&&x!==null||m||k+' is required'
+V.string=m=>_((x,k)=>T(x)||x instanceof String||Y(m,k,'a string'))
+V.hasLength=(v,m)=>_((x,k)=>x[l]==v||Y(m,k,v+` ${J(x)}s long`))
+V.minLength=(v,m)=>_((x,k)=>x[l]>=v||Y(m,k,`at least ${v} ${J(x)}s long`))
+V.maxLength=(v,m)=>_((x,k)=>x[l]<=v||Y(m,k,`at most ${v} ${J(x)}s long`))
+V.regex=(r,m)=>_((x,k)=>r.test(x)||m||k+` must match the regex `+r)
+V.number=m=>_((x,k)=>T(x,'number')&&x==x||Y(m,k,'a number'))
+V.bigint=m=>_((x,k)=>T(x,'bigint')||Y(m,k,'a bigint'))
+V.min=(v,m)=>_((x,k)=>x>=v||Y(m,k,'greater than '+v))
+V.max=(v,m)=>_((x,k)=>x<=v||Y(m,k,'less than '+v))
+V.boolean=m=>_((x,k)=>x===!0||x===!1||Y(m,k,'a boolean'))
+V.date=m=>_((x,k)=>x instanceof Date&&!isNaN(x)||Y(m,k,'a date'))
+V.array=(Z,m)=>_((x,k)=>Array.isArray(x)&&(Z?x.map((v,i)=>{if(T(v=Z(v,k+`[${i}]`)))throw new Error(v)})&&1:1)||Y(m,k,'a valid array'))
+export default V
---
+++ test.js
@@ -0,0 +1,29 @@
+import validate from './index.js';
+
+validate({
+  required: [validate.required()],
+  string: [validate.string()],
+  length: [validate.string(), validate.hasLength(8)],
+  minLength: [validate.string(), validate.minLength(8)],
+  maxLength: [validate.string(), validate.maxLength(8)],
+  regex: [validate.string(), validate.regex(/[a-z]+/g)],
+  array: [validate.array(validate.string()), validate.minLength(1)],
+  number: validate.number(),
+  min: [validate.number(), validate.min(3)],
+  max: [validate.number(), validate.max(3)],
+  boolean: validate.boolean(),
+  date: validate.date(),
+})({
+  required: true,
+  string: "this",
+  length: "long boi",
+  minLength: "longer boi",
+  maxLength: "smolboi",
+  regex: "aaaaa",
+  number: 2,
+  min: 4,
+  max: 3,
+  boolean: true,
+  date: new Date(),
+  array: ["string"],
+})
